50853bcc1b518632d0602dda7e12e2a21e3b31d0
Carlos Humberto Galván Perales <carloshgalvan95@gmail.com>
2022-07-22 17:29:03 -0500

    Modified PyPoll challenge syntax, added pie charts for votes

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -82,7 +82,7 @@ with open(file_to_save, "w") as txt_file:
 
     # Print the final vote count (to terminal)
     election_results = (
-        f"\nElection Results\n"
+        f"Election Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n\n"
@@ -128,11 +128,12 @@ with open(file_to_save, "w") as txt_file:
         # Retrieve vote count and percentage
         votes = candidate_votes.get(candidate_name)
         vote_percentage = float(votes) / float(total_votes) * 100
-        candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
+        
         # Print each candidate's voter count and percentage to the
         # terminal.
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n"
+            )
         print(candidate_results)
 
         #  Save the candidate results to our text file.
